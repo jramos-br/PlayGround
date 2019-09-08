9a0c05719e4cab86cc3c534ccb910dbae199992a
Jorge Ramos <jramos@pobox.com>
2019-09-08 02:04:20 -0300

    Bugcheck initial version

---
+++ Bugcheck/Bugtest3/Bugtest3.cpp
@@ -0,0 +1,42 @@
+// The Bugtest3 program.
+//
+// Tests the bugcheck macro.
+//
+// Copyright (c) 2019 Jorge Ramos (mailto jramos at pobox dot com)
+//
+// This is free software. Redistribution and use in source and binary forms,
+// with or without modification, for any purpose and with or without fee are
+// hereby permitted. Altered source versions must be plainly marked as such.
+//
+// If you find this software useful, an acknowledgment would be appreciated
+// but is not required.
+//
+// THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT ANY WARRANTY OR CONDITION.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE TO ANYONE
+// FOR ANY DAMAGES RELATED TO THIS SOFTWARE, UNDER ANY KIND OF LEGAL CLAIM.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "bugcheck.h"
+
+class Program
+{
+public:
+    Program(void)
+    {
+        printf("I'm a C++ program\n");
+    }
+
+    int main(int argc, char* argv[])
+    {
+        (void)argv;
+        bugcheck(argc == 1);
+        return EXIT_SUCCESS;
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    Program p;
+    return p.main(argc, argv);
+}
