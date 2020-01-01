eaa01eda4bc20c0f1f4bfbebba3dd690b4aa76f3
Jorge Ramos <jramos@pobox.com>
2020-01-01 03:24:28 -0300

    Update DbxListFiles

---
+++ DbxUtil/DbxListFiles/copyexe.cmd
@@ -0,0 +1,4 @@
+copy bin\Debug\DbxListFiles.* \\JRHM01\Jorge\TEMP\Dropbox
+copy bin\Debug\DbxListFiles.* \\JRNB01\Jorge\TEMP\Dropbox
+copy bin\Debug\DbxListFiles.* \\JRNB02\Jorge\TEMP\Dropbox
+pause
