18d01b822d662d0c7b61fbc905f40fb85ddc640d
Jorge Ramos <jramos@pobox.com>
2023-04-08 18:17:47 -0300

    Add ConsoleApp1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// A simple enum.
+    /// </summary>
+    public enum MyEnum
+    {
+        /// <summary>
+        /// A value.
+        /// </summary>
+        Value1,
+        /// <summary>
+        /// Another value.
+        /// </summary>
+        Value2,
+        /// <summary>
+        /// A third value.
+        /// </summary>
+        Value3
+    }
+
+    /// <summary>
+    /// A class.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// The Main method.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
