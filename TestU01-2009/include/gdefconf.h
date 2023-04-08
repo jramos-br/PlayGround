d1d9e237fedde554f6394954a02b6e9fe1fa0f6c
Jorge Ramos <jramos@pobox.com>
2023-04-08 16:56:14 -0300

    Add TestU01-2009 include directory

---
+++ TestU01-2009/include/gdefconf.h
@@ -0,0 +1,30 @@
+/* include/gdefconf.h.  Generated from gdefconf.h.in by configure.  */
+/* gdef.h  for ANSI C */
+#ifndef GDEFCONF_H
+#define GDEFCONF_H
+
+/* #undef HAVE_LEBOOL */
+
+#define HAVE_LONG_LONG 1
+
+#define HAVE_ERF 1
+
+/* #undef HAVE_RANDOM */
+
+#define HAVE_LGAMMA 1
+
+#define HAVE_GMP_H 1
+
+/* #undef HAVE_MATHEMATICA */
+
+/* #undef HAVE_SYS_UTSNAME_H */
+
+#define HAVE_UNISTD_H 1
+
+#define HAVE_STDINT_H 1
+
+#define HAVE_UINT32_T 1
+
+#define HAVE_UINT8_T 1
+
+#endif
