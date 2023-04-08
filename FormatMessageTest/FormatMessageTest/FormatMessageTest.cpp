9f97355ca43ceb2e3ddc59f0c1d76ccfb82d6f67
Jorge Ramos <jramos@pobox.com>
2023-04-08 18:03:32 -0300

    Add FormatMessageTest

---
+++ FormatMessageTest/FormatMessageTest/FormatMessageTest.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <tchar.h>
+
+#include "Win32IntlTest.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    _CRT_UNUSED(argc);
+    _CRT_UNUSED(argv);
+
+    PrintWin32IntlData();
+
+    return EXIT_SUCCESS;
+}
